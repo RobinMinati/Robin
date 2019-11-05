c1ab4e92bf944a0284fd44d74790f4b2330177bc
Robin <robin@gmail.com>
2019-11-05 16:01:00 +0000

    telechargement OK

--- src/get_fastq.sh
+++ src/get_fastq.sh
@@ -1,23 +1,30 @@
 #! /bin/bash
 
 # Hsapiens
-data="/home/rstudio/TP_RNAseq_Medical"
+data="/home/rstudio/disk/"
 mkdir -p $data
 cd $data
 mkdir -p sra_data
 cd sra_data
+#On fait un fichier sra_data dans /home/rstudio/disk/
 
-SRR="SRR3308950
-SRR3308951
-SRR3308952
+SRR="SRR3308956
+SRR3308957
+SRR3308963
+SRR3308964
 "
+#On definit SRR comme variable et on y associe les numeros de sequences sur le NCBI
 
 for srr in $SRR
 do
 # Produces one fastq file, single end data. 
-fastq-dump -h
 
+fastq-dump $srr -O /home/rstudio/disk/sra_data -X 100000 
+#pour chaque patient on telechanger les 100000 premieres reads et on les sauvegarde dans le dossier sra_data
+
+#fastq-dump -h
+#echo $srr
 # rename sequence names
-awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
-...
+#awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
+#...
 done
