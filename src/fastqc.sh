760ceaca373ddc14c2b1a6feca62ad2fded00e12
Robin Minati <robin@gmail.com>
2019-11-06 17:27:05 +0000

    ajout commentaires

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -8,22 +8,26 @@ mkdir -p $data
 cd $data
 mkdir -p sra_data
 cd sra_data
-#on cree le dossier 
+#on cree le dossier sra_data si il n'existe pas
+
 FASTQ=`ls $(pwd)`
-#je definis la variable FASTQ et j'y inclue les fichiers de sra-data
+#on pose la variable FASTQ comme étant la liste des fichiers de sra-data
 cd ..
 mkdir ConQual
+#je sors de sra_data et je crée un dossier ConQal
+
 cd sra_data
+
 for file in $FASTQ
 do
 echo $file
 
 fastqc $file -o /home/rstudio/disk/ConQual
-# Produces one fastq file, single end data. 
+#pour tous les fichiers de la variable, je run fastqc et je dirige l'output vers ConQual
 
+# Produces one fastq file, single end data. 
 #fastq-dump $srr -O /home/rstudio/disk/sra_data -X 100000 
 #pour chaque patient on telechanger les 100000 premieres reads et on les sauvegarde dans le dossier sra_data
-
 #fastq-dump 
 #echo $srr
 # rename sequence names
--- src/trimmomatic_jeu.sh
+++ src/trimmomatic_jeu.sh
@@ -1,23 +1,19 @@
 #! /bin/bash
 ## Run trimmomatic
 
-Pair=/home/rstudio/disk/sra_data/trimmomatic/pair
-UnPair=/home/rstudio/disk/sra_data/trimmomatic/unpair
-
-#Je définis deux variables correspondants aux chemin des dossiers pair et unpair
+Pair=/home/rstudio/disk/sra_data_jeu/trimmomatic_jeu/pair
+UnPair=/home/rstudio/disk/sra_data_jeu/trimmomatic_jeu/unpair
 
 data="/home/rstudio/disk/"
 mkdir -p $data
 cd $data
-cd sra_data
-mkdir -p trimmomatic
-cd trimmomatic
+cd sra_data_jeu
+mkdir -p trimmomatic_jeu
+cd trimmomatic_jeu
 mkdir -p pair
 mkdir -p unpair
-# si il n'existe pas, dans le dossier sra_data, je créé un nouveau dossier nommé trimmomatic que je divise en dossier pair et unpair
-
-cd /home/rstudio/disk/sra_data
-#je me place dans le dossier sra_data
+cd /home/rstudio/disk/sra_data_jeu
+#On fait un dossier trimmomatic 
 
 SRR="SRR3308956
 SRR3308957
@@ -33,7 +29,7 @@ SRR3308979
 SRR3308982
 SRR3308983
 "
-#je pose la variable SRR qui continet les differents SRR
+#On definit SRR comme variable et on y associe les numeros de sequences sur le NCBI
 
 for patient in $SRR;
 do
@@ -43,15 +39,6 @@ do
 	$Pair/$patient"_trimmed_paired_2.fastq" $UnPair/$patient"_trimmed_unpaired_2.fastq" -threads 7 \
 	ILLUMINACLIP:/home/rstudio/disk/FastaAdapteurs.fa:2:30:10 LEADING:22 SLIDINGWINDOW:4:22 MINLEN:25
 	
-#Pour chaque patient que contient SRR, on run Trimmomatic avec la fonction PE (pair ends). 
-#La ligne 41 sont les données prisent par le programme 
-#les lignes 42 et 43 sont les outputs et la nommenclarure 
-#on fait un illuminaclip pour enlever les adapeturs illumina et les séquences poly T (qu'on a resneignées dans le fichier FastaAdapteurs.fa)
-#on fait fait un leading pour enlever les premieres bases du read si elles sont en desous du treshold de qualité (22)
-#on fait in slidingdown pour couper la séquence quand la qualité moyenne dans la fenetre (4)tombe en dessous du seuil (22)
-#on fait un minlen pour retirer les reads qui font moins de 25 paires de bases 
-
-
 #ILLUMINACLIP:<fastaWithAdaptersEtc>:<seed mismatches>:<palindrome clip threshold>:<simple clip threshold>
 #SLIDINGWINDOW:<windowSize>:<requiredQuality>
 #java -jar <path to trimmomatic.jar> PE [-threads <threads] [-phred33 | -phred64] [-trimlog <logFile>] <input 1> <input 2> <paired output 1> <unpaired output 1> <paired output 2> <unpaired output 2> <step 1> ...
