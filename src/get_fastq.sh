038428a590f5fcc0598b94ef50e84aa347a663c3
Robin Minati <robin@gmail.com>
2019-11-06 17:55:57 +0000

    commentaires get fastq

--- src/get_fastq.sh
+++ src/get_fastq.sh
@@ -6,7 +6,7 @@ mkdir -p $data
 cd $data
 mkdir -p sra_data
 cd sra_data
-#On fait un fichier sra_data dans /home/rstudio/disk/
+#On fait un fichier sra_data dans /home/rstudio/disk/ si il n'existe pas
 
 SRR="SRR3308956
 SRR3308957
@@ -22,15 +22,14 @@ SRR3308979
 SRR3308982
 SRR3308983
 "
-#On definit SRR comme variable et on y associe les numeros de sequences sur le NCBI
+#On definit SRR comme une variable qui peut prendre la valeur des differents SRR
 
 for srr in $SRR
 do
-# Produces one fastq file, single end data. 
-
 fastq-dump $srr -O /home/rstudio/disk/sra_data --split-files -I 
 
-#pour chaque patient on telechanger les 100000 premieres reads et on les sauvegarde dans le dossier sra_data
+#pour chaque patient on telechanger tous les reads et on les sauvegarde dans le dossier sra_data en gardant les informations (-I)
+
 #-X 100000 => telecharger les 100000 premieres sequences
 #--split-files on sépare les sequences des deux extrémités du fragment -I (on garde le nom)
 #fastq-dump -h (avoir de l'aide)
@@ -41,7 +40,8 @@ awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print
 awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/2"}  else  { print $0} }' $srr"_2.fastq" > temp2.fastq
 mv temp1.fastq $srr'_1.fastq'
 mv temp2.fastq $srr'_2.fastq'
-
+#on divise ligne par ligne et on coupe les mots aprés les points. 
+#Si la ligne est impair on remplace le poit par un tiret et le numero aprés la coupure et un /1 ou /2. On nomme le temp1.fastq ou temp1.fastq et on les renomme par le SRR+extension _1.fastq ou _2.fastq
 #awk (modification ligne par ligne=> divise le ligne en morceaux) -F "\."(on coupe au viniveau d'un point caractere)  '{ if (NR%2 == 1 )si le numero de la signe est impair { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
 #...
 done
