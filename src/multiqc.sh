0aecb1c3ea6917aca97a75f9439e2002536935f5
Robin <robin@gmail.com>
2019-11-06 13:06:51 +0000

    combiné tout dans un dossier

--- src/multiqc.sh
+++ src/multiqc.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
-multiqc
+#multiqc
 
+data="/home/rstudio/disk/"
+mkdir -p $data
+cd $data
+mkdir -p ConQual
+mkdir -p ConQualSumm
+cd ConQual
+#on cree le dossier si il n'existe pas
 
+#FASTQ=`ls $(pwd)`
+#je definis la variable FASTQ et j'y inclue les fichiers de ConQual
+#for file in $FASTQ
+#do
+#cp $file"_ConQual"/* $data
+#fastqc $file -o $file"_ConQual"
+
+#cd /home/disk/sra_data/
+multiqc . -o /home/rstudio/disk/ConQualSumm
+
+done
