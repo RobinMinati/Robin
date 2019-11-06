db14195ab554e1bd9cb55248d7124d3f34ed900b
Robin Minati <robin@gmail.com>
2019-11-06 09:11:10 +0000

    Telechangement et nomenclature des données

--- src/get_fastq.sh
+++ src/get_fastq.sh
@@ -19,12 +19,20 @@ for srr in $SRR
 do
 # Produces one fastq file, single end data. 
 
-fastq-dump $srr -O /home/rstudio/disk/sra_data -X 100000 
-#pour chaque patient on telechanger les 100000 premieres reads et on les sauvegarde dans le dossier sra_data
+fastq-dump $srr -O /home/rstudio/disk/sra_data --split-files -I 
 
-#fastq-dump -h
-#echo $srr
+#pour chaque patient on telechanger les 100000 premieres reads et on les sauvegarde dans le dossier sra_data
+#-X 100000 => telecharger les 100000 premieres sequences
+#--split-files on sépare les sequences des deux extrémités du fragment -I (on garde le nom)
+#fastq-dump -h (avoir de l'aide)
+#echo $srr (afficher les srr)
 # rename sequence names
-#awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
+
+awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }' $srr"_1.fastq" > temp1.fastq
+awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/2"}  else  { print $0} }' $srr"_2.fastq" > temp2.fastq
+mv temp1.fastq $srr'_1.fastq'
+mv temp2.fastq $srr'_2.fastq'
+
+#awk (modification ligne par ligne=> divise le ligne en morceaux) -F "\."(on coupe au viniveau d'un point caractere)  '{ if (NR%2 == 1 )si le numero de la signe est impair { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
 #...
 done
