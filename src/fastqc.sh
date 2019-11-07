8e2d77390d9b92a35ce9c0a73f2c2e60866fae82
Robin Minati <robin@gmail.com>
2019-11-07 07:25:15 +0000

    création option 1 et 2 pour pair et impair

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -3,26 +3,43 @@
 
 
 # Hsapiens
+Unpair=/home/rstudio/disk/sra_data/trimmomatic/unpair
+Pair=/home/rstudio/disk/sra_data/trimmomatic/pair
 data="/home/rstudio/disk/"
 mkdir -p $data
 cd $data
 mkdir -p sra_data
 cd sra_data
+mkdir -p trimmomatic
+cd trimmomatic
+
+#OPTION 1
+#mkdir -p unpair
+#cd unpair
+
+#OPTION 2
+mkdir -p pair
+cd pair
+
 #on cree le dossier sra_data si il n'existe pas
 
 FASTQ=`ls $(pwd)`
 #on pose la variable FASTQ comme étant la liste des fichiers de sra-data
-cd ..
-mkdir ConQual
-#je sors de sra_data et je crée un dossier ConQal
+cd /home/rstudio/disk
+mkdir -p ConQual_CleanData
+#je sors de unpair et je crée un dossier ConQal pour l'analyse des data netoyé 
 
-cd sra_data
+#OPTION 1
+#cd $Unpair
+
+#OPTION 1
+cd $Pair
 
 for file in $FASTQ
 do
 echo $file
 
-fastqc $file -o /home/rstudio/disk/ConQual
+fastqc $file -o /home/rstudio/disk/ConQual_CleanData
 #pour tous les fichiers de la variable, je run fastqc et je dirige l'output vers ConQual
 
 # Produces one fastq file, single end data. 
